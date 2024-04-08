ddd11e63a402b3d192c2f5951ee451e891cc09de
ajain8616 <ajain8616@gmail.com>
2024-04-08 22:36:15 +0530

    Major Project

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const dbConnection = () => {
   mongoose
     .connect(process.env.MONGO_URI, {
-      dbName: "hospital_mangment_system",
+      dbName: "Medical Facility Administration Suite",
     })
     .then(() => {
       console.log("Connected to database!");
